2a1f7686c28bc5f8bf06b4798f820d2c0c289147
cc19931002 <1181018082@qq.com>
2018-06-01 16:01:12 +0800

    写timeFilter逻辑的一部分

--- src/main/java/cn/edu/bupt/DeviceFilter/TimeFilter.java
+++ src/main/java/cn/edu/bupt/DeviceFilter/TimeFilter.java
@@ -1,5 +1,10 @@
 package cn.edu.bupt.DeviceFilter;
 
+import cn.edu.bupt.common.model.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2018/5/24.
  * 往前回溯 forwardDays 天，如果这些天中的指定时间点有大于 threshold 的天数满足，则返回true
@@ -18,6 +23,25 @@ public class TimeFilter implements Filter{
     public double filter(DeviceFilterMetadata metadata) {
         //TODO 考虑往前推30天，在三十天中的当前时间点附近如果发生指定服务调用的天数为count1，在三十天中的当前时间点附近如果发生指定服务调用的次数和为count2
         // TODO 评价指标考虑设置为 （count1/30）*（30/(count2+1)）
+        long currentTime = System.currentTimeMillis();
+        final long oneDays = 1000*60*60*24;
+        List<Event> ls = new ArrayList<>();
+        //TODO 初始化ls
+        int countOfdays = 0;
+
+        int index = 0;
+
+        for(int i=30;i>0;i++){
+                long startTime = currentTime-i*oneDays;
+                Event ev = ls.get(index);
+                int happen = 0;
+                int totalHappen = 0;
+                while(ev.getTime()<startTime+30*60*1000){
+                    if(ev.getTime()>startTime-30*60*1000){
+
+                    }
+                }
+        }
         return 1.0;
     }
 }
---
+++ src/main/java/cn/edu/bupt/common/model/Event.java
@@ -0,0 +1,11 @@
+package cn.edu.bupt.common.model;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2018/6/1.
+ */
+@Data
+public class Event {
+    private long time;
+}

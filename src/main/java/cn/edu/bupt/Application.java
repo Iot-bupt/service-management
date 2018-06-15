475cc28de2c078472acaebfefeaf1f6845eddd93
youzi <liyou@bupt.edu.cn>
2018-06-15 13:24:28 +0800

    liyou：新增统一配置中心

--- src/main/java/cn/edu/bupt/Application.java
+++ src/main/java/cn/edu/bupt/Application.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by Administrator on 2018/4/10.
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("cn.edu.bupt")
 @MapperScan("cn.edu.bupt.mapper")
+@PropertySource({"classpath:disconf.properties"})
+@ImportResource({"classpath:disconf.xml"})//引入disconf
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
